aaec8d9e45bcd77b55ec4d445e69b580f3ee3b17
Khushi Kumari <132158593+Sonna21@users.noreply.github.com>
2023-12-25 18:04:47 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,12 @@
+const app = require('express')();
+
+const PORT = 8080;
+
+app.listen(
+    PORT,
+    () => console.log('Server running on ${PORT}')
+);
+
+app.get('/fruits', (req, res) => {
+    res.send(Khushi);
+});
